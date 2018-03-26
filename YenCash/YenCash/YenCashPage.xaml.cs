2ee3721eb8edaef1bb696c27eabada77c34d6511
Rabbit <venkatasivaprasadreddypulagam@gmail.com>
2018-03-26 18:49:03 +0530

    Committing changes

---
+++ YenCash/YenCash/YenCashPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace YenCash
+{
+    public partial class YenCashPage : ContentPage
+    {
+        public YenCashPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
